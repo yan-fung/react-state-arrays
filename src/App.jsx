8daaaa75cdaced160dcf074ad0e571cf024a8550
Yan Fung <fungmingyan@hotmail.com>
2023-02-05 09:19:23 +0000

    Added favourite state variable, handleFavourite function and handle NewJoke function. Passed functions as props to Joke and JokeForm components

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,39 @@
 import './App.css'
 import Joke from './Joke'
-import JokeForm from './JokeForm'
-
-import { useState } from "react"
+import JokeForm from './JokeForm';
+import { useState } from 'react';
 
 function App() {
-  const [jokes, setJokes] = useState([
+  const [favourite, setFavourite] = useState(1);
+
+  const jokes = [
     {
       id: 1,
-      text: "I'm afraid for the calendar. Its days are numbered.",
-      likes: 0
+      text: "I'm afraid for the calendar. Its days are numbered."
     },
     {
       id: 2, 
-      text: "I used to be addicted to soap, but I'm clean now.",
-      likes: 0
+      text: "I used to be addicted to soap, but I'm clean now."
     }
-  ])
+  ]
 
-  const handleAddJoke = (text) => {
-    const joke = {
-      text,
-      id: self.crypto.randomUUID(),
-      likes: 0
-    }
-    // jokes.push(joke)
-    setJokes([joke, ...jokes])
-    console.log("New Joke:", text)
+  const handleFavourite = (id) => {
+    setFavourite(id)
   }
-
-  const handleDeleteJoke = (id) => {
-    setJokes(jokes.filter(joke => joke.id !== id))
-    console.log("delete joke", id)
-  }
-
-  const handleLike = (id) => {
-    setJokes(jokes.map(joke => {
-      if (joke.id === id) {
-        return {...joke, likes: joke.likes + 1}
-      } else {
-        return joke
-      }
-    }))
-  }
-
-  const handleDislike = (id) => {
-    setJokes(jokes.map(joke => {
-      if (joke.id === id) {
-        return {...joke, likes: joke.likes - 1}
-      } else {
-        return joke
-      }
-    }))
-  }
-
-  const handleSort = () => {
-    setJokes([...jokes].sort((a, b) => b.likes - a.likes))
+  
+  const handleNewJoke = (text) => {
+    console.log("new joke", text)
   }
 
   return (
     <div className="">
       <h1>Dad Jokes</h1>
 
-      <button onClick={handleSort}>Sort</button>
-
-      <JokeForm onAddJoke={handleAddJoke} />
-
       {jokes.map(joke => (
-        <Joke 
-        onDelete={handleDeleteJoke} 
-        key={joke.id} 
-        onLike={handleLike}
-        onDislike={handleDislike}
-        {...joke} />
+        <Joke onFavourite={handleFavourite} favourite={favourite === joke.id} key={joke.id} id={joke.id} text={joke.text} />
       ))}
 
+      <JokeForm onNewJoke={handleNewJoke} />
 
     </div>
   )
