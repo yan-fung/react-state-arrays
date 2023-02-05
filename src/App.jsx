92a165d162fb9f5ed4aca1c32443b44ae18e39e1
Yan Fung <fungmingyan@hotmail.com>
2023-02-05 11:01:33 +0000

    Managed and added handleLike and handleDislike function in App component and passed the function as props to Joke component. Created sort function to sort the like count

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,74 @@ import JokeForm from './JokeForm';
 import { useState } from 'react';
 
 function App() {
-  const [favourite, setFavourite] = useState(1);
 
-  const jokes = [
+  const [jokes, setJokes] = useState([
     {
       id: 1,
-      text: "I'm afraid for the calendar. Its days are numbered."
+      text: "I'm afraid for the calendar. Its days are numbered.",
+      likes: 0
     },
     {
       id: 2, 
-      text: "I used to be addicted to soap, but I'm clean now."
+      text: "I used to be addicted to soap, but I'm clean now.",
+      likes: 0
     }
-  ]
+  ])
 
-  const handleFavourite = (id) => {
-    setFavourite(id)
+  const handleLike = (id) => {
+    setJokes(jokes.map((joke => {
+      if (joke.id === id) {
+        return {...joke, likes: joke.likes + 1}
+      } else {
+        return joke
+       }
+    })))
+  }
+
+  const handleDislike = (id) => {
+    setJokes(jokes.map((joke => {
+      if (joke.id === id) {
+        return {...joke, likes: joke.likes - 1}
+      } else {
+        return joke
+       }
+    })))
   }
   
-  const handleNewJoke = (text) => {
+  const handleAddJoke = (text) => {
+    const joke = {
+      text,
+      id: self.crypto.randomUUID,
+      likes: 0
+    }
+    setJokes([joke, ...jokes])
     console.log("new joke", text)
   }
+
+  const handleDelete = (id) => {
+    setJokes(jokes.filter(joke => joke.id !== id))
+    console.log("delete joke:", id)
+  }
+
+const handleSort = () => {
+  setJokes([...jokes].sort((a, b) => b.likes - a.likes))
+}
+
 
   return (
     <div className="">
       <h1>Dad Jokes</h1>
-
+      <button onClick={handleSort}>Sort</button>
+      <JokeForm onAddJoke={handleAddJoke} />
       {jokes.map(joke => (
-        <Joke onFavourite={handleFavourite} favourite={favourite === joke.id} key={joke.id} id={joke.id} text={joke.text} />
+        <Joke
+          onDelete={handleDelete}
+          key={joke.id} 
+          onLike={handleLike}
+          onDislike={handleDislike}
+          {...joke}
+        />
       ))}
-
-      <JokeForm onNewJoke={handleNewJoke} />
-
     </div>
   )
 }
