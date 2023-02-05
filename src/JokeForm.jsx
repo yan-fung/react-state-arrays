c0711107c802e7c2eba045e9b2f3bdbc10ba4054
Yan Fung <fungmingyan@hotmail.com>
2023-02-05 09:39:10 +0000

    Added text state variable, handleSubmit function. Returns a form with input. Added text as value and called a callback function for onchange to set the text

--- src/JokeForm.jsx
+++ src/JokeForm.jsx
@@ -1,24 +1,20 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function JokeForm({ onAddJoke }) {
-  const [text, setText] = useState("")
+function JokeForm({ onNewJoke }) {
+  const [text, setText] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    onAddJoke(text)
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    onNewJoke(text)
     setText("")
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="text">New Joke</label>
-      <input
-        type="text"
-        id="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button type="submit">Add Joke</button>
-    </form>
-  )
+     <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Enter a joke" value={text} onChange={e => setText(e.target.value)} />
+        <button type="submit">Submit</button>
+     </form>
+    )
 }
+
+export default JokeForm;
