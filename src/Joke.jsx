df03279d769421a10b3adc2eea468e655955fdfe
Yan Fung <fungmingyan@hotmail.com>
2023-02-05 09:31:21 +0000

    Added handleLike, handleDislike, handleFavourite functions. Added like counts, favourite option and event handlers in return block

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -1,22 +1,35 @@
-import { useState } from "react"
+import { useState } from 'react';
 
-export default function Joke({ id, text, onDelete, likes, onLike, onDislike }) {
+export default function Joke({ id, text, favourite, onFavourite }) {
+
+  const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
+
 
   const handleLike = () => {
-    onLike(id)
+    const newLike = likes + 1;
+    setLikes(newLike)
+    console.log(`like id: ${id}, totalLikes ${newLike}`)
   }
 
   const handleDislike = () => {
-    onDislike(id)
+    const newDislike = dislikes + 1;
+    setDislikes(newDislike)
+    console.log(`dislike id: ${id}, totalDislike ${newDislike}`)
+  }
+
+  const handleFavourite = () => {
+    onFavourite(id)
   }
 
   return (
     <div>
       <p>{text}</p>
-      <p>Likes: {likes}</p>
+      <p>Likes: {likes - dislikes}</p>
+      <p>Favourite: {favourite ? "Yes" : "No"}</p>
       <button onClick={handleLike}>👍</button>
       <button onClick={handleDislike}>👎</button>
-      <button onClick={() => onDelete(id)}>delete</button>
+      <button onClick={handleFavourite}>Favourite</button>
     </div>
   )
 }
